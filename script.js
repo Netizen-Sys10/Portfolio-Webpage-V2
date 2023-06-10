5952e5dd77cc8d9fe2a50ccb0fb5364ce6159284
Avi <simon.ghide1@gmail.com>
2023-06-10 02:22:44 +0200

    Updated the page and fixed container sizing.

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 // add to the project the acive id
 // the current displayed content needs to be changed to and lose its active id
 
+$(window).on('resize', () => {
+
+});
+
+
 const linkOne = $('#link-1');
 const projectOne =  $('#project-1');
 const projectTwo =  $('#project-2');
@@ -10,19 +15,41 @@ const projectThird =  $('#project-3');
 const firstMessage = $('.first-message');
 
 linkOne.on('click', function () {
-    $('.project-show').removeClass('project-show')
+
+    $('.project-show').removeClass('project-show');
     projectOne.addClass('project-show');
+    $('.overview').css({'height': projectOne.height()});
+    console.log( $('.overview').height());
 });
 
 
 
 
 $('#link-2').on('click', function () {
-    $('.project-show').removeClass('project-show')
+  
+    $('.project-show').removeClass('project-show');
     projectTwo.addClass('project-show');
+    $('.overview').css({'height': projectTwo.height()});
+    console.log( $('.overview').height());
 });
 
+
 $('#link-3').on('click', function () {
-    $('.project-show').removeClass('project-show')
+ 
+    $('.project-show').removeClass('project-show');
     projectThird.addClass('project-show');
+    $('.overview').css({'height': projectThird.height()});
+    console.log( $('.overview').height());
 });
+
+
+
+$('#logo-link').on('click', function () {
+    $('.project-show').removeClass('project-show');
+    $('.first-message').addClass('project-show');
+    $('.overview').css({'height':$('.first-message').height() });
+})
+
+$(window).on('resize', () => { 
+    $('.overview').css({'height': $('.project-show').height()});
+})
